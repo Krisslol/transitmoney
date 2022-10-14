8fafa0c0251ac3343f3f05254986e47dc087af3a
Kristina <volosnikova-k@yandex.ru>
2022-10-14 20:59:09 +0500

    change some variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 fun main() {
-    val amount = 5000
-    val comission:Double = amount * 0.75 / 100
-    val totalPrice = comission + amount
-    if (comission<35) {
-        println(amount + 35)
-    } else
-        println("Итоговая сумма вместе с комиссией:  $totalPrice")
+    val amount = 400
+    var comission:Double = amount * 0.0075
+    val minComission = 35
+    val res = if(minComission>comission) amount + minComission else amount + comission
+
+    println(res)
+
 }
 
 
